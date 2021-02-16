bc8a7c27d0973a40c825828e19a0378bf82f9a64
nmhummel <natalie.hummel@gmail.com>
2021-02-15 22:49:16 -0600

    player is now Dalek image and looks in direction of arrow key pressed

--- frontend/index.js
+++ frontend/index.js
@@ -23,13 +23,16 @@
 // const gameBoard = document.getElementById('gameBoard');
 // //const context = gameBoard.getContext('2d');
 //     gameBoard.width = 500;
-//     gameBoard.height = 600;
+//     gameBoard.height = 500;
 // const cellSize = 100;
 // //const cellGap = 0;
 // //const gameGrid = [];
 
 const player = document.getElementById('player');
 const gameBoard = document.getElementById('gameBoard');
+gameBoard.width = 500;
+gameBoard.height = 500;
+
 
 document.addEventListener("keyup", function(e) {
     if (e.key === "ArrowLeft") {
@@ -52,6 +55,7 @@ let playerTop = 0;
 
 function movePlayerRight() {
     playerLeft += 100;
+    player.src = "images/d-right.png"
     player.style.left = playerLeft + 'px';
     if (playerLeft >= 400) {
         playerLeft -= 100;
@@ -60,6 +64,7 @@ function movePlayerRight() {
 
 function movePlayerLeft() {
     playerLeft -= 100;
+    player.src = "images/d-left.png"
     player.style.left = playerLeft + 'px';
     if (playerLeft < 0) {
         playerLeft += 100
@@ -68,6 +73,7 @@ function movePlayerLeft() {
 
 function movePlayerDown() {
     playerTop += 100;
+    player.src = "images/d-down.png"
     player.style.top = playerTop + 'px';
     if (playerTop >= 400) {
         playerTop -=100;
@@ -76,8 +82,42 @@ function movePlayerDown() {
 
 function movePlayerUp() {
     playerTop -= 100;
+    player.src = "images/d-up.png"
     player.style.top = playerTop + 'px';
     if (playerTop <=0) {
         playerTop += 100;
     }
 }
+
+const cellSize = 100;
+const gameGrid = [];
+
+class Cell {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+    draw() {
+        //gameBoard.strokeStyle = "white";
+        //gameBoard.strokeRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function createGrid() {
+    for (let y = cellSize; y < gameBoard.height; y += cellSize) {
+        for (let x = 0; x < gameBoard.width; x += cellSize) {
+            gameGrid.push(new Cell(x,y));
+        }
+    }
+}
+createGrid();
+console.log(gameGrid);
+
+// function handleGameGrid() {
+//     for (let i = 0; i < gameGrid.length; i++) {
+//         gameGrid[i].draw();
+//     }
+// }
+// handleGameGrid(); 
