2ae8ca5cf014407214ebfe388b122e9cef3ff481
nmhummel <natalie.hummel@gmail.com>
2021-02-15 19:11:33 -0600

    added arrow key events and functions; not working right now

--- frontend/index.js
+++ frontend/index.js
@@ -1,90 +1,81 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetchScores()
+   
     //fetchBoards()
 })
 
-const BASE_URL = 'http://localhost:3000'
-// read - scoreboard (scores, username)
-function fetchScores() {
-    fetch(`${BASE_URL}/scores`)
-    .then(resp => resp.json())
-    .then(scores => {console.log("OBJ", scores)}
-    //     for (const score of scores) {
-    //         console.log("OBJ", score)
-    //         let s = new Score(score.id, score.moves, score.username, score.board_id)
-    //         console.log("obj", s)
-    //     }
-    // }
-    )
-}
-// create - new score
-// update - update score when repeating a board
 
-//create board
-const canvas = document.getElementById('canvas1');
-const context = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 600;
 
 // player avatar assignment
-const upMove = new Image(); // Create new img element
-upMove.src = 'd-up.png'; // Set source path
-const downMove = new Image(); 
-downMove.src = 'd-down.png'; 
-const rightMove = new Image(); 
-rightMove.src = 'd-right.png'; 
-const leftMove = new Image(); 
-leftMove.src = 'd-left.png'; 
+// const upMove = new Image(); // Create new img element
+//     upMove.src = 'd-up.png'; // Set source path
+// const downMove = new Image(); 
+//     downMove.src = 'd-down.png'; 
+// const rightMove = new Image(); 
+//     rightMove.src = 'd-right.png'; 
+// const leftMove = new Image(); 
+//     leftMove.src = 'd-left.png'; 
+// const tardisCard = new Image(95, 95);
+//     tardisCard = "tardis-icon";
 
  
+// //create board
+// const gameBoard = document.getElementById('gameBoard');
+// //const context = gameBoard.getContext('2d');
+//     gameBoard.width = 500;
+//     gameBoard.height = 600;
+// const cellSize = 100;
+// //const cellGap = 0;
+// //const gameGrid = [];
 
-// global variables
-const cellSize = 100;
-const cellGap = 0;
-const gameGrid = [];
+const dalek = document.getElementById('dalek');
+const gameBoard = document.getElementById('gameBoard');
 
-//game board
-const controlSquare = {
-    width: cellSize,
-    height: cellSize,
+let dalekLeft = 0;
+let dalekTop = 0;
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDalekLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDalekRight();
+    }
+    else if (e.key === "ArrowDown") {
+        moveDalekDown();
+    }
+    else if (e.key === "ArrowUp") {
+        moveDalekUp();
+  }); 
+
+function moveDalekLeft() {
+    dalekLeft -= 100;
+    dalek.style.left = dalekLeft + 'px';
+    if (dalekLeft <= 0) {
+        dalekLeft += 100
+    }
+} 
+
+function moveDalekRight() {
+    dalekLeft += 100;
+    dalek.style.left = dalekLeft + 'px';
+    if (dalekLeft >= 400) {
+        dalekLeft -= 100;
+    }
 }
 
-class Cell {
-    constructor(x,y) {
-        this.x = x;
-        this.y = y;
-        this.width = cellSize;
-        this.height = cellSize;
-    }
-    draw() {
-        context.strokeStyle = "black";
-        context.strokeRect(this.x, this.y, this.width, this.height);
+function moveDalekDown() {
+    dalekTop += 100;
+    dalek.style.top = dalekTop + 'px';
+    if (dalekTop >= 500) {
+        dalekTop -=100;
     }
 }
 
-function createGrid() {
-    for (let y = cellSize; y < canvas.height; y += cellSize) {
-        for (let x = 0; x < canvas.width; x += cellSize) {
-            gameGrid.push(new Cell(x,y));
-        }
-    }
+function moveDalekUp() {
+    dalekTop -= 100;
+    dalek.style.top = dalekTop + 'px'
+    if (dalekTop <=0) {
+        dalekTop += 100;
+    } 
 }
-createGrid();
-console.log(gameGrid);
-
-function handleGameGrid() {
-    for (let i = 0; i < gameGrid.length; i++) {
-        gameGrid[i].draw();
-    }
-}
-handleGameGrid();  
-
-
-// function animate() {
-//     context.fillStyle = "blue";
-//     context.fillRect(0,0, controlSquare.width, controlSquare.height);
-//     requestAnimationFrame(animate);
-// }
-
-// animate();
--- frontend/src/score.js
+++ frontend/src/score.js
@@ -6,5 +6,21 @@ class Score {
     this.board_id = board_id
     }
 
-    
+    const BASE_URL = 'http://localhost:3000'
+// read - scoreboard (scores, username)
+function fetchScores() {
+    fetch(`${BASE_URL}/scores`)
+    .then(resp => resp.json())
+    .then(scores => {console.log("OBJ", scores)}
+    //     for (const score of scores) {
+    //         console.log("OBJ", score)
+    //         let s = new Score(score.id, score.moves, score.username, score.board_id)
+    //         console.log("obj", s)
+    //     }
+    // }
+    )
+}
+// create - new score
+// update - update score when repeating a board
+
 }
