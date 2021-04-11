183e0acdeeca0f68443de60abbe7d7d337a46a03
nmhummel <natalie.hummel@gmail.com>
2021-04-10 22:14:53 -0500

    heroku

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://dazzling-cray-23c3c6.netlify.app'
 
     resource '*',
       headers: :any,
